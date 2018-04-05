4d97bc9877ffac195880872ba18be74616565ca6
helene <helene.labriet@novitom.com>
2018-04-05 14:57:10 +0200

    Adding  file support

--- spytIO.py
+++ spytIO.py
@@ -4,3 +4,19 @@ SpytLab speckle python lab
 Author: helen labriet & Emmanuel brun
 Date: April 2018
 """
+
+import EdfFile as edf
+from PIL import Image
+import numpy as np
+
+def openImage(filename):
+    if filename.endswith('.edf'):
+        im = edf.EdfFile(filename, access='rb')
+        imarray = im.GetData(0)
+    else :
+        imarray = Image.open(filename)
+        imarray = np.array(imarray)
+
+    return imarray
+
+
